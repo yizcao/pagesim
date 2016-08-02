d4f1992e1d1ce53bd751d16acaa4d1ad9268813c
Frank Cao <caoyizheng@yeah.net>
2016-08-03 00:28:21 +0800

    update by Frank

--- src/com/action/LoginAction.java
+++ src/com/action/LoginAction.java
@@ -1,5 +1,12 @@
 package com.action;
 
-public class LoginAction {
+import com.opensymphony.xwork2.ActionSupport;
 
+public class LoginAction extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute();
+	}
 }
